4f4d071d13661ea66fd5aae7a9aa4a025ecc8510
Dan Kibaara <kibaara.gamedev@gmail.com>
2024-10-18 18:19:38 +0300

    Adds styles to buttons via index.js

--- src/index.js
+++ src/index.js
@@ -1,161 +1,136 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 
+  const createTaskForm = document.getElementById("create-task-form");
+  let tskDescrptn = document.getElementById("tsk-descrptn");
+  const tasks = document.getElementById("tasks");
+  const formData = new FormData(createTaskForm);
+  let submitBtn = document.getElementById("submit-btn");
 
-  const createTaskForm= document.getElementById('create-task-form')
-  let tskDescrptn= document.getElementById('tsk-descrptn')
-  const tasks= document.getElementById('tasks')
-  const formData=new FormData(createTaskForm);
-  let submitBtn= document.getElementById('submit-btn')
-
-
-
-createTaskForm.addEventListener("submit",(e)=>{
+  createTaskForm.addEventListener("submit", (e) => {
     e.preventDefault();
-    
 
-if(tskDescrptn.value==="")
-{
-  //chek if tsk inpt is empty
-  alert("Can't insert empty task") 
-}
-else{
-  for(let [name,value] of formData){
-    value=tskDescrptn.value //destruct value  property frm formData
+    if (tskDescrptn.value === "") {
+      //chek if tsk inpt is empty
+      alert("Can't insert empty task");
+    } else {
+      for (let [name, value] of formData) {
+        value = tskDescrptn.value; //destruct value  property frm formData
 
-     let newTsk= document.createElement('li')
-     let tskContent=document.createElement('p')
-     let tskActions =document.createElement('div')
-     let deleteTsk= document.createElement('button') // for delete actions
-     let editTsk=document.createElement('button'); //for edit
-     let tskPriority=document.createElement('select') // for  priority selection
+        let newTsk = document.createElement("li");
+        let tskContent = document.createElement("p");
+        let tskActions = document.createElement("div");
+        let deleteTsk = document.createElement("button"); // for delete actions
+        let editTsk = document.createElement("button"); //for edit
+        let tskPriority = document.createElement("select"); // for  priority selection
 
-     let options=['Priority','High','Medium','Low']
+        let options = ["Priority", "High", "Medium", "Low"];
 
-     //dynamically adds  options to select elemnt,  tskPriority
-     options.forEach((optn)=>{
-     let optionElement= document.createElement('option')
-     optionElement.value=optn.toLowerCase();
-     optionElement.textContent=optn
-    
-      tskPriority.appendChild(optionElement)   //priority 
-   
+        //dynamically adds  options to select elemnt,  tskPriority
+        options.forEach((optn) => {
+          let optionElement = document.createElement("option");
+          optionElement.value = optn.toLowerCase();
+          optionElement.textContent = optn;
 
-      
-     })
+          tskPriority.appendChild(optionElement); //priority
+        });
 
-     
-     //actions styles
-     tskActions.style.display='flex'
-     tskActions.style.justifyContent='space-between'
-     tskActions.style.width='64px'
-     
-     tskActions.appendChild(editTsk) //append edit & delete actions
-     tskActions.append(deleteTsk)
+        //actions styles
+        tskActions.style.display = "flex";
+        tskActions.style.justifyContent = "space-between";
+        tskActions.style.width = "auto";
 
-     //edit icn styles
-     editTsk.style.border="1px solid black"
-     editTsk.style.height="24px"
-     editTsk.style.width="24px"
-     editTsk.style.textAlign="center"
-     editTsk.innerHTML=('i')
-     
-      
-     //delete icn styles
-     deleteTsk.style.border="1px solid black"
-     deleteTsk.style.height="24px"
-     deleteTsk.style.width="24px"
-     deleteTsk.style.textAlign="center"
-     deleteTsk.innerHTML=('X')
+        tskActions.appendChild(editTsk); //append edit & delete actions
+        tskActions.append(deleteTsk);
 
+        //edit icn styles
+        editTsk.style.border = "1px solid black";
+        editTsk.style.height = "40px";
+        editTsk.style.padding = "0 24px";
+        editTsk.style.width = "auto";
+        editTsk.style.textAlign = "center";
+        editTsk.innerHTML = "Edit";
 
-      //ALL FUNCTIONS START HERE!!
+        //delete icn styles
+        deleteTsk.style.border = "1px solid black";
+        deleteTsk.style.height = "40px";
+        deleteTsk.style.padding = "0 24px";
+        deleteTsk.style.width = "auto";
+        deleteTsk.style.textAlign = "center";
+        deleteTsk.innerHTML = "Delete";
 
+        //ALL FUNCTIONS START HERE!!
 
- //Set Priority
- tskPriority.addEventListener('click',()=>{
-  let selectedPriority = tskPriority.value;
-        
-  switch(selectedPriority){
-    case("high"):{
-     newTsk.style.backgroundColor="red";
-     break
-     
-    }
-    case("medium"):{
-      newTsk.style.backgroundColor="orange";
-      break
-    }
-    case("low"):{
-      newTsk.style.backgroundColor="green";
-      break
-    }
-    default:
-      newTsk.style.backgroundColor="#387478"
-      break
-    
+        //Set Priority
+        tskPriority.addEventListener("click", () => {
+          let selectedPriority = tskPriority.value;
 
-  }
-  
- })
+          switch (selectedPriority) {
+            case "high": {
+              newTsk.style.backgroundColor = "red";
+              break;
+            }
+            case "medium": {
+              newTsk.style.backgroundColor = "orange";
+              break;
+            }
+            case "low": {
+              newTsk.style.backgroundColor = "green";
+              break;
+            }
+            default:
+              newTsk.style.backgroundColor = "#387478";
+              break;
+          }
+        });
 
+        //edit tsk
+        editTsk.addEventListener("click", (e) => {
+          if (e.target.tagName === "BUTTON") {
+            //set  tsk descptn value to tsk content
+            tskDescrptn.value = tskContent.innerHTML;
+            e.target.disabled = true; //disable edit btn
 
+            //edit task value
 
-     
+            submitBtn.style.display = "none"; //hide  submit btn
 
-     //edit tsk
-     editTsk.addEventListener('click',(e)=>{
-      if(e.target.tagName==="BUTTON"){
-        //set  tsk descptn value to tsk content
-        tskDescrptn.value= tskContent.innerHTML;
-        e.target.disabled=true //disable edit btn
+            let updateTskBtn = document.createElement("button"); //create updt tsk btn
+            updateTskBtn.innerHTML = "Update Task";
+            updateTskBtn.style.backgroundColor = "violet";
+            updateTskBtn.style.color = "white";
 
-        //edit task value
-        
-      submitBtn.style.display="none"  //hide  submit btn
-     
+            updateTskBtn.addEventListener("click", () => {
+              tskContent.innerHTML = tskDescrptn.value; // Update task description
+              tskDescrptn.value = ""; // Clear input field
+              submitBtn.style.display = "block"; // Re-enable submit button
+              submitBtn.style.opacity = "1"; // Reset opacity
+              updateTskBtn.remove(); // Remove update button
+              e.target.disabled = false; //Re-enable edit btn
+            });
 
-      let updateTskBtn= document.createElement('button') //create updt tsk btn
-      updateTskBtn.innerHTML=('Update Task')
-      updateTskBtn.style.backgroundColor="violet"
-      updateTskBtn.style.color="white"
-      
-      updateTskBtn.addEventListener('click', () => {
-        tskContent.innerHTML = tskDescrptn.value; // Update task description
-        tskDescrptn.value = ""; // Clear input field
-        submitBtn.style.display="block"; // Re-enable submit button
-        submitBtn.style.opacity = '1'; // Reset opacity
-        updateTskBtn.remove(); // Remove update button
-        e.target.disabled=false //Re-enable edit btn
+            createTaskForm.appendChild(updateTskBtn); //add updt tsk btn to form
+          }
+        });
+        //delete tsk
+        deleteTsk.addEventListener("click", (e) => {
+          if (e.target.tagName == "BUTTON") {
+            e.target.closest("li").remove(); //remove list element
+          }
+        });
 
-    });
+        // Adds task to the tasks list
 
-      createTaskForm.appendChild(updateTskBtn) //add updt tsk btn to form
+        tskContent.innerHTML = value; //get input value
+        newTsk.append(tskContent);
+        newTsk.append(tskPriority);
+        newTsk.append(tskActions);
+        tasks.appendChild(newTsk);
+
+        tskDescrptn.value = ""; //make tsk input empty
+
+        console.log(`${name}= ${value}`);
       }
-     })
-      //delete tsk
-     deleteTsk.addEventListener('click' ,(e)=>{
-       
-      if(e.target.tagName=="BUTTON"){
-        e.target.closest('li').remove() //remove list element
-      }
-      
-     })
-
-     
-
-
-     // Adds task to the tasks list
-
-     tskContent.innerHTML=value  //get input value
-     newTsk.append(tskContent)
-     newTsk.append(tskPriority)
-     newTsk.append(tskActions)
-     tasks.appendChild(newTsk)
-     
-     tskDescrptn.value="" //make tsk input empty
-     
-      console.log(`${name}= ${value}`)
-  }}
-
-  })})
+    }
+  });
+});
